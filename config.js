5fa794e02ec1e3215a1cb1479ecc7780a6c69e32
sue445 <sue445@sue445.net>
2013-01-17 01:06:03 +0900

    add my jenkins setting

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    job_urls : [
+        "http://ci.sue445.net/jenkins/job/azusaar/",
+        "http://ci.sue445.net/jenkins/job/jubeatplusplus/",
+        "http://ci.sue445.net/jenkins/job/kulib/",
+        "http://ci.sue445.net/jenkins/job/s3tiger/",
+    ],
+    reload_seconds : 30
+};
+
